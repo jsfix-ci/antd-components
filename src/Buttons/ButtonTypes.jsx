b75443c4239bdc0650a54fd597c73cef7a699e67
danij <scripty@outlook.de>
2019-10-16 20:36:33 +0200

    change default button type

--- src/Buttons/ButtonTypes.jsx
+++ src/Buttons/ButtonTypes.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {Button} from "./Button";
 
 export const AddButton = (props) => {
-    const {onClick, color = 'green', size = 'small', icon = 'plus', text = 'Add', restProps} = props;
+    const {onClick, color = 'green', size = 'default', icon = 'plus', text = 'Add', restProps} = props;
     return <Button size={size} onClick={onClick} icon={icon} color={color} {...restProps} text={text} />
 };
 
 export const EditButton = (props) => {
-    const {onClick, color = 'blue', size = 'small', icon = 'edit', text = 'Edit', restProps} = props;
+    const {onClick, color = 'blue', size = 'default', icon = 'edit', text = 'Edit', restProps} = props;
     return <Button size={size} onClick={onClick} icon={icon} color={color} {...restProps} text={text} />
 };
 
 export const DeleteButton = (props) => {
-    const {onClick, color = 'red', size = 'small', icon = 'delete', text = 'Delete', restProps} = props;
+    const {onClick, color = 'red', size = 'default', icon = 'delete', text = 'Delete', restProps} = props;
     return <Button size={size} onClick={onClick} icon={icon} color={color} {...restProps} text={text} />
 };
